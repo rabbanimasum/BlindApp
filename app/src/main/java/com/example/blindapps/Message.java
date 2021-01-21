61bce354ad416c15faf46172f4b9a2c8a4c74669
Masum <https://github.com/rabbanimasum/Myapps>
2021-01-22 02:52:16 +0600

    Visually impriaed assistance

---
+++ app/src/main/java/com/example/blindapps/Message.java
@@ -0,0 +1,12 @@
+package com.example.blindapps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Message extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+    }
+}
---
+++ app/src/main/java/com/example/blindapps/ReadPdf.java
@@ -0,0 +1,12 @@
+package com.example.blindapps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReadPdf extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_pdf);
+    }
+}
